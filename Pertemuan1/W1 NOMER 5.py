4170ab95920b2c47645058ef61fbdd6ce7d98c36
Ulima Faiza Nabila <ulimafaizanabila@mail.ugm.ac.id>
2025-02-15 19:29:45 +0700

    W1 Latihan 5

---
+++ Pertemuan1/W1 NOMER 5.py
@@ -0,0 +1,9 @@
+print("Menghitung Luas Lingkaran")
+r = int(input("Masukkan jari-jari :"))
+Luas = 3.14 * r * r
+print("Luas lingkaran adalah", Luas)
+print("Menghitung Keliling Lingkaran")
+r = int(input("Masukkan jari-jari :"))
+Keliling = 2 * 3.14 * r
+print(" Keliling lingkaran adalah", Keliling)
+
